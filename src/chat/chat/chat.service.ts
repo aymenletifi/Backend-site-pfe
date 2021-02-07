c213358008266a617685709a664ba7df78165e43
unknown <aymen.letifi@live.com>
2021-02-07 18:31:37 +0100

    login , register and chat app

--- src/chat/chat/chat.service.ts
+++ src/chat/chat/chat.service.ts
@@ -8,12 +8,24 @@ export class ChatService {
 
     constructor(@InjectModel(Room.name) private roomModel: Model<RoomDocument>){}
 
-    async findRoom(user1: string, user2:string): Promise<Room | undefined> {
+    async findRoom(user1: string, user2:string): Promise<RoomDocument | undefined> {
         return this.roomModel.findOne({ $or: [{ user1: user1, user2:user2}, { user1: user2, user2: user1 }]});
       }
 
     async addRoom(user1 : string, user2: string){
         this.roomModel.create({user1: user1, user2:user2});
       }
+
+    async findMessages(user1: string, user2:string):Promise<any>{
+        const room = await this.findRoom(user1, user2);
+        return room.messages
+    }
+
+    async findRoomById(id: string){
+        return this.roomModel.findById(id);
+    }
+
+    
+
 
 }
